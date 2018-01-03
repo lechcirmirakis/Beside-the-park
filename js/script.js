01d05551fd47a235097d4d1d1d0b65bd50a13a7c
Lech Cirmirakis <lech.cirmirakis@go2.pl>
2018-01-03 20:52:39 +0100

    BStP add iphon animation with delay

--- js/script.js
+++ js/script.js
@@ -5,24 +5,34 @@ $(() => {
   $(window).scroll(() => {
 
     if ($(window).scrollTop() > 80) {
-      $('.navbar').css({
+      $('.c-navbar').css({
         'height': '100px',
       });
 
-      $('.navbar__fadeIn-menu').css({
+      $('.c-navbar__leftside-menu').css({
         'top': '100px',
       });
     }
 
   });
 
-  // sied menu
+  window.setTimeout(() => {
+    let mainContent = document.querySelector('.c-main-content');
+    let iphon = document.querySelector('.js-iphone-img');
+    mainContent.classList.add('animated_2s');
+    mainContent.classList.add('fadeIn');
+    iphon.classList.add('animated_2s');
+    iphon.classList.add('fadeIn');
 
-  let menuTrig = document.querySelector('.menu-trigger');
-  let menuWrap = document.querySelector('.navbar__fadeIn-menu');
+  }, 1500);
+
+  // side menu
+
+  let menuTrig = document.querySelector('.c-navbar__menu-icon');
+  let menuWrap = document.querySelector('.c-navbar__leftside-menu');
 
   menuTrig.addEventListener('click', (event) => {
-    menuWrap.classList.toggle('navbar__fadeIn-menu--visible');
+    menuWrap.classList.toggle('js-leftside-menu--visible');
   });
 
 });
