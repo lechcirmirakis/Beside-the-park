e9d43b47c2ce07be77d85d27ed4a56197326f433
Lech Cirmirakis <lech.cirmirakis@go2.pl>
2018-01-04 10:47:39 +0100

    BStP add namespaces to row and container

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // delay for animation
   window.setTimeout(() => {
-    let mainContent = document.querySelector('.c-main-content');
+    let mainContent = document.querySelector('.js-main-content');
     let iphon = document.querySelector('.js-iphone-img');
     iphon.classList.add('animated_1-5s');
     iphon.classList.add('fadeInRightBig');
@@ -11,8 +11,8 @@ document.addEventListener("DOMContentLoaded", () => {
   }, 1000);
 
   // side menu
-  let menuTrig = document.querySelector('.c-navbar__menu-icon');
-  let menuWrap = document.querySelector('.c-navbar__leftside-menu');
+  let menuTrig = document.querySelector('.js-navbar__menu-icon');
+  let menuWrap = document.querySelector('.js-navbar__leftside-menu');
 
   menuTrig.addEventListener('click', (event) => {
     menuWrap.classList.toggle('js-leftside-menu--visible');
